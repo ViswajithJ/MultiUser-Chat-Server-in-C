cae89f904d020177b326c05f912566ff63e03459
Viswajith J <viswajith.j.official@gmail.com>
2024-03-05 22:33:17 +0530

    Update: [+] Define Macros [+] Handle Disconnection of clients properly [+] Handle errors and exits [+] Handle max clients case in server [+] Cleanup code in general

--- TCP/server.c
+++ TCP/server.c
@@ -3,10 +3,12 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
-#include <sys/types.h>
 #include <netinet/in.h>
 #include <pthread.h>
 
+#define PORT 60000
+#define BUFFER_SIZE 256
+#define MAX_CLIENTS 5
 
 int client_sockfds[5];  // array of client socket fds
 int client_count = 0;
@@ -14,31 +16,22 @@ int client_count = 0;
 void *client_chat(void *client_sock)
 {
 	int c_sock = *(int *)client_sock;
-	char name[256];
-	char buffer[256];
-
-	memset(buffer, 0, sizeof(buffer));
- 	strcpy(buffer, "Enter name: \0");
- 	send(c_sock, buffer, strlen(buffer), 0);
-
- 	memset(name, 0, sizeof(name));
- 	recv(c_sock, name, 256, 0);
-
- 	// name[strlen(name)-1] = ' : ';
+	char buffer[BUFFER_SIZE];
 
  	while(1)
  	{
  		memset(buffer, 0, sizeof(buffer));
- 		int read_bytes = recv(c_sock, buffer, 256, 0);
+ 		int read_bytes = recv(c_sock, buffer, BUFFER_SIZE, 0);
  		if ((!strcmp(buffer, "quit\n")) || (read_bytes == 0)) {
  			close(c_sock);
  			printf("Connection disconnected\n");
- 			break;
+ 			client_count--;
+ 			if (!client_count)
+ 				exit(1);
  		} else if (read_bytes < 0) {
  			printf("recv error \n");
  			break;
  		} else {
- 			strcat(name, buffer);
  			for (int i = 0; i < client_count; i++) {
  				if (client_sockfds[i] != c_sock) {
  					int e = send(client_sockfds[i], buffer, strlen(buffer), 0);
@@ -48,10 +41,8 @@ void *client_chat(void *client_sock)
  			}
  		}
  	}
-
-
-
-
+ 	free(client_sock);
+    pthread_exit(NULL);
 }
 
 int main()
@@ -62,37 +53,47 @@ int main()
 
 	//SOCKET
 	serv_sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (serv_sockfd < 0)
+	if (serv_sockfd < 0) {
 		printf("Socket error\n");
-
+		exit(1);
+	}
+	
 	//BIND
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	serv_addr.sin_port = htons(60000);
+	serv_addr.sin_port = htons(PORT);
 
 			 //(server struct, address of struct, size of struct)
-	if (bind(serv_sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+	if (bind(serv_sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
 		printf("Binding error \n");
-
-
+		exit(1);
+	}
 	//LISTEN
 	       //server fd, max requests(clients))
-	listen(serv_sockfd, 5);
-	printf("server is listening on port 60000\n");
+	listen(serv_sockfd, MAX_CLIENTS);
+	printf("server is listening on port %d\n", PORT);
 
 	//ACCEPT
 	int client_len = sizeof(struct sockaddr_in);
 
-	int i = 0;
-	while(client_sockfds[i++] = accept(serv_sockfd, (struct sockaddr *)&client_addr, &client_len)) {
-		printf("Client %d connected \n", i-1);
-		pthread_t client_thread;
+	while(1) {
+		if ((client_sockfds[client_count] = accept(serv_sockfd, (struct sockaddr *)&client_addr, &client_len)) < 0)
+		{
+			printf("Accept error\n");
+			exit(1);
+		}
 		client_count++;
-		int *client_sock = (int *)malloc(sizeof(int));
-		*client_sock = client_sockfds[i - 1];
-
-						 //address of thread, 
-		if(pthread_create(&client_thread, NULL, client_chat, (void *) client_sock) < 0) 
-			printf("Thread error \n");
+		if (client_count < MAX_CLIENTS) {
+			printf("Client %d connected \n", client_count-1);
+			pthread_t client_thread;
+			int *client_sock = (int *)malloc(sizeof(int));
+			*client_sock = client_sockfds[client_count - 1];
+							 //address of thread, 
+			if(pthread_create(&client_thread, NULL, client_chat, (void *) client_sock) < 0) 
+					printf("Thread error \n");
+		} else {
+			printf("Max Clients Reached. Connection Rejected. Bye\n");
+			close(client_sockfds[client_count]);
+		}
 	}
 }
